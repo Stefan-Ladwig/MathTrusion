6e4b8de293a38e7171365e8e370e1de520442be7
Stefan Ladwig <ladwig.stefan@gmx.de>
2022-03-23 21:04:16 +0100

    dumped rotation function and used scipy for that, made all output 3 dimensional, code cleanup

--- base_shapes.py
+++ base_shapes.py
@@ -1,20 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
 
 
 def plt_polygon(vertices):
-    fig, ax = plt.subplots()
+    _, ax = plt.subplots()
+    l = len(vertices)
     for i in range(len(vertices)):
-        plt.plot([vertices[i, 0], vertices[(i + 1) % len(vertices), 0]],\
-                [vertices[i, 1], vertices[(i + 1) % len(vertices), 1]], 'r-')  
+        plt.plot([vertices[i, 0], vertices[(i + 1) % l, 0]],\
+                 [vertices[i, 1], vertices[(i + 1) % l, 1]], 'r-')  
     ax.set_aspect('equal')
     plt.show()
-
-
-def rotate_vector(vector, rot_angle):
-    rot_mat = np.array([[np.cos(rot_angle), -np.sin(rot_angle)],\
-                        [np.sin(rot_angle), np.cos(rot_angle)]])
-    return np.matmul(rot_mat, vector)
 
 
 def vertices_from_function(func, input_values=np.linspace(-1,1)):
@@ -24,27 +20,29 @@ def vertices_from_function(func, input_values=np.linspace(-1,1)):
 
 def n_gon(n):
     def func(phi):
-        return [np.cos(phi), np.sin(phi)]
+        return [np.cos(phi), np.sin(phi), 0]
     phi = np.linspace(0, 2 * np.pi, n, endpoint=False)
     return vertices_from_function(func, phi)
 
 
 def star(spikiness=2.5, num_spikes=5):
-    rot_angle = np.pi / num_spikes
-    inner_polygon = n_gon(num_spikes)
-    outer_polygon = spikiness * inner_polygon
-    inner_polygon = np.array([rotate_vector(v, rot_angle)\
-                              for v in inner_polygon])
-    vertices = np.zeros((2 * num_spikes, 2))
-    for i in range(num_spikes):
-        vertices[2 * i] = outer_polygon[i]
-        vertices[2 * i + 1] = inner_polygon[i]
+    rot_vec = [0, 0, np.pi / num_spikes]
+    rotation = R.from_rotvec(rot_vec)
+
+    outer_polygon = n_gon(num_spikes)
+    inner_polygon = outer_polygon / spikiness
+    inner_polygon = rotation.apply(inner_polygon)
+
+    vertices = np.zeros((2 * num_spikes, 3))
+    vertices[0::2] = outer_polygon
+    vertices[1::2] = inner_polygon
     return vertices
 
 
 def squircle(exponent=4):
     def func(x):
-        return [x, (1 - abs(x)**exponent)**(1 / exponent)]
+        return [x, (1 - abs(x)**exponent)**(1 / exponent), 0]
+
     x_values = np.linspace(-1, 0, 200)
     upper_left = vertices_from_function(func, x_values)
     upper_right = upper_left.copy()
