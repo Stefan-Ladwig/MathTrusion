4744ada25b58e65fd7376e544a178828caaa8a6e
Stefan Ladwig <ladwig.stefan@gmx.de>
2022-03-23 21:05:13 +0100

    added collection for some basic functions usable as patch_func

---
+++ func_collection.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def circle(x, scale=1):
+    return scale * np.array([np.cos(x), np.sin(x), 0])
+
+
+def helix(x, scale=1, period=2*np.pi):
+    x = (x / 2 * np.pi) * period
+    return circle(x, scale=scale) + np.array([0, 0, x])
+
+
+def spiral(x, scale=1):
+    return x * circle(x, scale=scale)
+
+
+def line(x, scale=1):
+    return scale * np.array([0, 0, x])
+
+
+def parabola(x, scale=1, width=2):
+    return scale * np.array([x, ((1 / width) * x)**2, 0])
