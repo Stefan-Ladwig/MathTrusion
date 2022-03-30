7645cecbd018ec0822e2318c4f7ef77482d40a3b
Stefan Ladwig <ladwig.stefan@gmx.de>
2022-03-31 01:21:08 +0200

    adjusted to changes in func_collection and base_shapes

--- examples.py
+++ examples.py
@@ -1,6 +1,7 @@
 from matplotlib import projections
 import base_shapes
 import func_collection
+from func_collection import func_dict
 from triangular_mesh import tri_mesh
 import numpy as np
 from mayavi import mlab
@@ -16,7 +17,9 @@ def plot_matplotlib(x, y, z, triangles):
     ax.set_facecolor('black')
     ax.set_box_aspect((np.ptp(x), np.ptp(y), np.ptp(z)))
     fig.tight_layout()
+
     triang = mtri.Triangulation(x, y, triangles=triangles)
+
     ax.plot_trisurf(triang, z, cmap=plt.cm.rainbow,\
                     edgecolors='grey', linewidths=0.1)
     plt.show()
@@ -51,7 +54,7 @@ def screw():
     shape_2d = base_shapes.n_gon(3)
     scale_func = lambda _: 1
     rot_func = lambda x: 4 * x
-    path_func = func_collection.line
+    path_func = func_collection.line()
 
     return tri_mesh(shape_2d, scale_func, rot_func, path_func,\
                     start=start, end=end, num_steps=num_steps)
@@ -65,7 +68,21 @@ def sphere():
     shape_2d = base_shapes.circle()
     scale_func = lambda x: np.sqrt(1 - (x)**2)
     rot_func = lambda _: 0
-    path_func = func_collection.line
+    path_func = func_collection.line()
+
+    return tri_mesh(shape_2d, scale_func, rot_func, path_func,\
+                    start=start, end=end, num_steps=num_steps)
+
+
+def cube():
+    start = -1 / np.sqrt(2)
+    end = 1 / np.sqrt(2)
+    num_steps = 2
+
+    shape_2d = base_shapes.n_gon(4)
+    scale_func = lambda _: 1
+    rot_func = lambda _: 0
+    path_func = func_collection.line()
 
     return tri_mesh(shape_2d, scale_func, rot_func, path_func,\
                     start=start, end=end, num_steps=num_steps)
@@ -79,7 +96,7 @@ def star_helix():
     shape_2d = base_shapes.star(spikiness=1.5)
     scale_func = lambda x: np.log(1 + x)
     rot_func = lambda x: x
-    path_func = lambda x: func_collection.helix(x, dist=1)
+    path_func = func_collection.helix(dist=1)
 
     return tri_mesh(shape_2d, scale_func, rot_func, path_func,\
                     start=start, end=end, num_steps=num_steps)
@@ -93,7 +110,7 @@ def squircle_circle():
     shape_2d = base_shapes.squircle(6, detail=25)
     scale_func = lambda _: 0.4
     rot_func = lambda x: x
-    path_func = func_collection.circle
+    path_func = func_collection.circle()
 
     return tri_mesh(shape_2d, scale_func, rot_func, path_func,\
                     start=start, end=end, num_steps=num_steps)
@@ -107,7 +124,7 @@ def triangle_parabola():
     shape_2d = base_shapes.n_gon(3)
     scale_func = lambda _: 1
     rot_func = lambda x: x * np.sin(x)
-    path_func = func_collection.parabola
+    path_func = func_collection.parabola()
 
     return tri_mesh(shape_2d, scale_func, rot_func, path_func,\
                     start=start, end=end, num_steps=num_steps)
@@ -119,13 +136,13 @@ def mobius_loop():
     num_steps = 100
 
     shape_2d = np.array([[-1, 0, 0], [1, 0, 0]])
-    scale_func = lambda _: 0.3
-    rot_func = lambda x: x
-    path_func = lambda x: np.array([0, np.cos(x), np.sin(x)])
+    scale_func = func_dict['s/r']['constant'](0.3)
+    rot_func = func_dict['s/r']['linear']()
+    path_func = func_dict['p']['circle'](orientation='yz')
 
     return tri_mesh(shape_2d, scale_func, rot_func, path_func,\
                     start=start, end=end, num_steps=num_steps)
 
 
 if __name__ == '__main__':
-   plot_matplotlib(*star_helix())
+   plot_matplotlib(*mobius_loop())
