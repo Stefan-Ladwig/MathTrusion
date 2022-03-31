20a99444d986bb04d9523c80c8ee0c0b0330403b
Stefan Ladwig <ladwig.stefan@gmx.de>
2022-03-31 18:44:41 +0200

    moved all text/button elements to left side, 3d plot now fills right side

--- GUI_visual.py
+++ GUI_visual.py
@@ -3,11 +3,7 @@ from matplotlib.pyplot import margins
 from func_collection import func_dict
 from base_shapes import shape_dict
 
-SYMBOL_UP =    '▲'
-SYMBOL_DOWN =  '▼'
-
 section_names = ['Base', 'Path', 'Scaling', 'Rotation', 'Intervall']
-
 
 
 def create_window():
@@ -30,7 +26,7 @@ def create_window():
     choices_sections = {
         s: [[sg.T(s, font=('fixed', 22), pad=(0, 16)), sg.Push(),\
              sg.Combo(list_elements[s], key=f'-{s}-', size=combo_size,\
-                      default_value=list_elements[s][:1],\
+                      default_value=list_elements[s][:1], enable_events=True,\
                       font=('fixed', 22), readonly=True)]]
         for s in section_names[:4]
     }
@@ -56,30 +52,24 @@ def create_window():
     ]
 
     choices_column.append(
-        [sg.Push(), sg.B('show preview', key='-BUTTON preview-'), sg.Push()]
+        [sg.Push(), sg.B('preview', key='-BUTTON preview-', size=(14, 1)),\
+         sg.Push(), sg.B('save', key='-BUTTON create-', size=(14, 1)), sg.Push(),\
+         sg.Push(), sg.B('view in mayavi', key='-BUTTON mayavi-', size=(14, 1)), sg.Push()]
     )
 
-    preview_sections = {
-        'title': [sg.Push(), sg.T('PREVIEW', font=('fixed', 22)), sg.Push()],
-        'plot': [sg.Push(), sg.Canvas(key='-PREVIEW Plot-'), sg.Push()],
-        'create': [sg.Push(), sg.B('create modell', key='-PREVIEW Button-'), sg.Push()]
-    }
-
     preview_column = [
-        [sg.Column([val], key=f'-PREVIEW Column {key}-',\
-                   expand_y=True, expand_x=True)]
-        for key, val in preview_sections.items()
+        [sg.Push(), sg.Canvas(key='-PREVIEW Plot-'), sg.Push()]
     ]
 
     layout = [
         [
             sg.Column(choices_column,\
                       expand_y=True, expand_x=True,\
-                      pad=(10,6)),
+                      pad=((10, 60),(0, 6))),
             sg.VSeparator(),
             sg.Column(preview_column,\
                       expand_y=True, expand_x=True,\
-                      pad=(20,6)),
+                      pad=(20,0)),
         ]
     ]
     
